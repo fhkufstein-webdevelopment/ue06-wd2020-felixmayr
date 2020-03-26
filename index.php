8a08593dbf6c90897ad72f36ef8a543520773b7d
Felix Mayr <felixmayr99@gmail.com>
2020-03-26 12:20:52 +0100

    Aufgabe 1

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
 
-//@TODO insert your code here
+require_once('includes/classes/Database.php');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ue6_db');
+define('DB_USER', 'ue6_user');
+define('DB_PASS', 'Hello123');
+
+$db = new Database();
+$cryptedPassword = password_hash('testpassword', PASSWORD_BCRYPT);
+$username = "peter";
+
+$cryptedPassword = $db->escapeString($cryptedPassword);
+$username = $db->escapeString($username);
+
+// $sql = "INSERT INTO user(name,`password`) VALUES('" . $username . "','" . $cryptedPassword . "')";
+// $db->query($sql);
+
+$sql = "SELECT * FROM user WHERE name='" . $username . "'";
+$result = $db->query($sql);
+if ($db->numRows($result) > 0)
+{
+    $row = $db->fetchAssoc($result);
+    if (password_verify("testpassword", $row['password'])) {
+        echo "User: " . $username . ", ID: " . $row['id'] . ", Passwort: testpassword";
+    } else {
+        echo "User found but wrong PW!";
+    }
+} else {
+    echo "No User found.";
+}
